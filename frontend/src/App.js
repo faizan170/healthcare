ef72e368f7dfa6cf4224ae38e5d9d070c267799d
malik102651 <maliknaseer102651@gmail.com>
2022-10-07 13:35:50 +0500

    first commit

---
+++ frontend/src/App.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+function App() {
+  return (
+    <div>
+
+      <h1 className="bg-slate-600 text-white">React App</h1>
+     
+    </div>
+  );
+}
+
+export default App;
